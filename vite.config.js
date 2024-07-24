5748fb1960d0e7c093625c8e553bb75e787f19bf
acz19 <acz1992@hotmail.co.uk>
2024-07-24 18:11:48 +0200

    Update build/outputdirectory in vite to dist

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	plugins: [react()],
 	//Add build if you are going to use a Git-based (Github or CodeCommit) deployement
 	build: {
-		outDir: "build",
+		outDir: "dist",
 	},
 	// Not sure is necessary
 	/* resolve: {
