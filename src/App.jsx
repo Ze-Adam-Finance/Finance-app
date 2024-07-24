c0e958fa14aa3b41e3726427bb2ca5153d86c077
acz19 <acz1992@hotmail.co.uk>
2024-07-24 22:58:29 +0200

    Add Data Table, and sort out aesthetic of App homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Searchbar from "./components/Searchbar";
 import Title from "./components/Title";
-import { Container, Box } from "@mui/material";
+import DataTable from "./components/DataTable";
+import { Container, Box, Divider } from "@mui/material";
 
 const App = () => {
 	return (
@@ -10,10 +11,18 @@ const App = () => {
 				display: "flex",
 				flexDirection: "column",
 				alignItems: "center",
+				pt: 4, // Adding padding at the top to space out from the top of the container
 			}}
 		>
 			<Title />
 			<Searchbar />
+			<Divider sx={{ width: "100%", my: 4 }} />{" "}
+			{/* Full width divider with margin bottom */}
+			<Box sx={{ width: "100%" }}>
+				{" "}
+				{/* Margin top to create space above the DataTable */}
+				<DataTable />
+			</Box>
 		</Container>
 	);
 };
