c792e101d4a0c2738883bb2f2773e14c4c681839
José Travassos <jmar.trav@gmail.com>
2024-07-24 23:41:58 +0200

    functioning search

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import Searchbar from "./components/Searchbar";
 import Title from "./components/Title";
+import { Container, Box } from "@mui/material";
 
 const App = () => {
 	return (
-		<>
+		<Container
+			sx={{
+				minHeight: "100vh",
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+			}}
+		>
 			<Title />
 			<Searchbar />
-		</>
+		</Container>
 	);
 };
 
