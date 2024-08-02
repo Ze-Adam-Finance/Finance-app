ce422a57958e8b8e112958a6a0b4c182b1867186
acz19 <acz1992@hotmail.co.uk>
2024-08-02 11:27:50 +0200

    Add Logo, make it responsive, make compatible with dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
 	const [chartData, setChartData] = useState([]);
 	const [incomeStatementData, setIncomeStatementData] = useState([]);
 	const [ratiosData, setRatiosData] = useState([]);
+	const theme = useTheme(); // Get the current theme (light or dark mode)
 
 	useEffect(() => {
 		if (selectedCompany) {
@@ -24,8 +25,6 @@ const App = () => {
 		}
 	}, [selectedCompany]);
 
-	const theme = useTheme();
-
 	return (
 		<Container
 			sx={{
@@ -36,13 +35,19 @@ const App = () => {
 				pt: 4,
 			}}
 		>
-			<Box display="flex" mt={1} justifyContent="flex-end">
+			<Box
+				sx={{
+					position: "fixed",
+					top: 16,
+					right: 16,
+					zIndex: 1000,
+				}}
+			>
 				<DarkModeToggle />
 			</Box>
 			<Box
 				sx={{
 					display: "flex",
-
 					alignItems: "center",
 					flexDirection: {
 						xs: "column",
@@ -62,6 +67,18 @@ const App = () => {
 						width: "100%",
 					}}
 				>
+					<img
+						src="/blackswan.png" // Path to your PNG logo
+						alt="Black Swan Logo"
+						style={{
+							maxWidth: "100px", // Default size
+							maxHeight: "100px", // Default size
+							filter:
+								theme.palette.mode === "dark"
+									? "invert(1)"
+									: "none", // Invert colors for dark mode
+						}}
+					/>
 					<Typography
 						component={"span"}
 						sx={{
@@ -72,7 +89,6 @@ const App = () => {
 					>
 						<span style={{ fontWeight: 600 }}>Black&nbsp;</span>{" "}
 						<span>Swan</span>
-						
 					</Typography>
 				</Box>
 
--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -4,11 +4,13 @@ import {
 	Brightness7 as Brightness7Icon,
 } from "@mui/icons-material";
 import { useTheme } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
 import { useDarkMode } from "../hooks/useDarkMode.js";
 
 const DarkModeToggle = () => {
 	const theme = useTheme(); // Get the current theme (light or dark mode)
 	const darkMode = useDarkMode(); // Get the toggle function for dark mode
+	const isLessThan1400px = useMediaQuery("(max-width:1400px)"); // Check if the screen width is less than 1400px
 
 	return (
 		<IconButton
@@ -24,24 +26,28 @@ const DarkModeToggle = () => {
 		>
 			{theme.palette.mode === "dark" ? (
 				<Box display="flex" alignItems="center">
-					<Typography
-						className="mode-text"
-						style={{ marginRight: 8 }}
-						variant="body2"
-					>
-						Light Mode
-					</Typography>
+					{!isLessThan1400px && (
+						<Typography
+							className="mode-text"
+							style={{ marginRight: 8 }}
+							variant="body2"
+						>
+							Light Mode
+						</Typography>
+					)}
 					<Brightness7Icon />
 				</Box>
 			) : (
 				<Box display="flex" alignItems="center">
-					<Typography
-						className="mode-text"
-						style={{ marginRight: 8 }}
-						variant="body2"
-					>
-						Dark Mode
-					</Typography>
+					{!isLessThan1400px && (
+						<Typography
+							className="mode-text"
+							style={{ marginRight: 8 }}
+							variant="body2"
+						>
+							Dark Mode
+						</Typography>
+					)}
 					<Brightness4Icon />
 				</Box>
 			)}
